13925ec251ff08e2e81f20c6d3b9eace86b61fc9
root <root@ATTCLI13.PCCOMPONENTES>
2016-09-28 21:53:30 +0200

    Genración de funciones Auxiliares con Twig.

--- src/AppBundle/Twig/HelperVistas.php
+++ src/AppBundle/Twig/HelperVistas.php
@@ -11,22 +11,30 @@ class HelperVistas extends \Twig_Extension
         return array ('generateTable' => new \Twig_Function_Method($this, 'generateTable'));    
     }
     
-    public function generateTable($num_colums, $num_rows)
-    {
-        $table = "<table class='table'> ";
+    public function generateTable($resultSet) {
+
+        $longitud = count($resultSet) - 1;
         
-        for ($i = 0 ; $i< $num_colums ; $i++)
-        {    
+
+        $table = "<table class='table' border=1> ";
+
+        for ($i = 0; $i < $longitud; $i++) 
+        {
+            $valores = array_values($resultSet[$i]);
             $table .= "<tr>";
-            
-            for($j=0; $j< $num_rows; $j++)
+
+            for ($j = 0; $j < 2; $j++) 
             {
-                $table .= "<td> COL </td>";
+                $table .= "<td> ";
+                $table .= $valores[$j];
+                $table .="</td>";
             }
-        }    
+        }
+        
+  
         return $table;
     }
-    
+
     public function getName() 
     {
         return "app_bundle";
